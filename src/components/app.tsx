62b2e9c881637f510cb5883c891bd36ad41e943a
phosphene47 <phosphene47@chiya.dev>
2020-10-18 00:56:07 +1100

    Initial commit

---
+++ src/components/app.tsx
@@ -0,0 +1,28 @@
+import { FunctionalComponent, h } from "preact";
+import { Route, Router, RouterOnChangeArgs } from "preact-router";
+
+import Home from "../routes/home";
+import Profile from "../routes/profile";
+import NotFoundPage from "../routes/notfound";
+import Header from "./header";
+
+const App: FunctionalComponent = () => {
+  let currentUrl: string;
+  const handleRoute = (e: RouterOnChangeArgs) => {
+    currentUrl = e.url;
+  };
+
+  return (
+    <div id="app">
+      <Header />
+      <Router onChange={handleRoute}>
+        <Route path="/" component={Home} />
+        <Route path="/profile/" component={Profile} user="me" />
+        <Route path="/profile/:user" component={Profile} />
+        <NotFoundPage default />
+      </Router>
+    </div>
+  );
+};
+
+export default App;
