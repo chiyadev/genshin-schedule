eefc92a32e200be347e356ed71866f8726dfe2a6
phosphene47 <phosphene47@chiya.dev>
2020-10-19 01:26:37 +1100

    Add github link to report missing items

--- src/routes/customize/help.tsx
+++ src/routes/customize/help.tsx
@@ -4,6 +4,18 @@ const Help = () => {
   return (
     <div className="text-xs text-gray-600">
       <div>Select items to be added to your farming schedule.</div>
+      <div>
+        If there are any missing items, please{" "}
+        <a
+          href={`https://github.com/chiyadev/genshin-schedule/issues/new?title=${encodeURIComponent(
+            "bug: Missing item in database {insert item name}"
+          )}`}
+          className="font-bold"
+        >
+          create an issue
+        </a>{" "}
+        on GitHub.
+      </div>
     </div>
   );
 };
