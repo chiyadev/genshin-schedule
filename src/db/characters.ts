1b290456c05053ce019597c3235b352251288f2d
phosphene47 <phosphene47@chiya.dev>
2020-10-24 15:45:52 +1100

    Add common ascension materials

--- src/db/characters.ts
+++ src/db/characters.ts
@@ -7,166 +7,199 @@ import {
   Resistance,
   TalentMaterial
 } from "./talentMaterials";
+import {
+  CommonMaterial,
+  FatuiInsignia,
+  HilichurlArrowhead,
+  HilichurlMask,
+  SamachurlScroll,
+  Slime,
+  TreasureHoarderInsignia,
+  WhopperflowerNectar
+} from "./commonMaterials";
 
 export type Character = {
   type: "Character";
   name: string;
   wiki: string;
   talentMaterial: TalentMaterial;
+  commonMaterial: CommonMaterial;
 };
 
 export const Amber: Character = {
   type: "Character",
   name: "Amber",
   wiki: "https://genshin-impact.fandom.com/wiki/Amber",
-  talentMaterial: Freedom
+  talentMaterial: Freedom,
+  commonMaterial: HilichurlArrowhead
 };
 
 export const Barbara: Character = {
   type: "Character",
   name: "Barbara",
   wiki: "https://genshin-impact.fandom.com/wiki/Barbara",
-  talentMaterial: Freedom
+  talentMaterial: Freedom,
+  commonMaterial: SamachurlScroll
 };
 
 export const Beidou: Character = {
   type: "Character",
   name: "Beidou",
   wiki: "https://genshin-impact.fandom.com/wiki/Beidou",
-  talentMaterial: Gold
+  talentMaterial: Gold,
+  commonMaterial: TreasureHoarderInsignia
 };
 
 export const Bennett: Character = {
   type: "Character",
   name: "Bennett",
   wiki: "https://genshin-impact.fandom.com/wiki/Bennett",
-  talentMaterial: Resistance
+  talentMaterial: Resistance,
+  commonMaterial: TreasureHoarderInsignia
 };
 
 export const Chongyun: Character = {
   type: "Character",
   name: "Chongyun",
   wiki: "https://genshin-impact.fandom.com/wiki/Chongyun",
-  talentMaterial: Diligence
+
+  talentMaterial: Diligence,
+  commonMaterial: HilichurlMask
 };
 
 export const Diluc: Character = {
   type: "Character",
   name: "Diluc",
   wiki: "https://genshin-impact.fandom.com/wiki/Diluc",
-  talentMaterial: Resistance
+  talentMaterial: Resistance,
+  commonMaterial: FatuiInsignia
 };
 
 export const Fischl: Character = {
   type: "Character",
   name: "Fischl",
   wiki: "https://genshin-impact.fandom.com/wiki/Fischl",
-  talentMaterial: Ballad
+  talentMaterial: Ballad,
+  commonMaterial: HilichurlArrowhead
 };
 
 export const Jean: Character = {
   type: "Character",
   name: "Jean",
   wiki: "https://genshin-impact.fandom.com/wiki/Jean",
-  talentMaterial: Resistance
+  talentMaterial: Resistance,
+  commonMaterial: HilichurlMask
 };
 
 export const Kaeya: Character = {
   type: "Character",
   name: "Kaeya",
   wiki: "https://genshin-impact.fandom.com/wiki/Kaeya",
-  talentMaterial: Ballad
+  talentMaterial: Ballad,
+  commonMaterial: TreasureHoarderInsignia
 };
 
 export const Keqing: Character = {
   type: "Character",
   name: "Keqing",
   wiki: "https://genshin-impact.fandom.com/wiki/Keqing",
-  talentMaterial: Prosperity
+  talentMaterial: Prosperity,
+  commonMaterial: WhopperflowerNectar
 };
 
 export const Lisa: Character = {
   type: "Character",
   name: "Lisa",
   wiki: "https://genshin-impact.fandom.com/wiki/Lisa",
-  talentMaterial: Ballad
+  talentMaterial: Ballad,
+  commonMaterial: Slime
 };
 
 export const Mona: Character = {
   type: "Character",
   name: "Mona",
   wiki: "https://genshin-impact.fandom.com/wiki/Mona",
-  talentMaterial: Resistance
+  talentMaterial: Resistance,
+  commonMaterial: WhopperflowerNectar
 };
 
 export const Ningguang: Character = {
   type: "Character",
   name: "Ningguang",
   wiki: "https://genshin-impact.fandom.com/wiki/Ningguang",
-  talentMaterial: Prosperity
+  talentMaterial: Prosperity,
+  commonMaterial: FatuiInsignia
 };
 
 export const Noelle: Character = {
   type: "Character",
   name: "Noelle",
   wiki: "https://genshin-impact.fandom.com/wiki/Noelle",
-  talentMaterial: Resistance
+  talentMaterial: Resistance,
+  commonMaterial: HilichurlMask
 };
 
 export const Qiqi: Character = {
   type: "Character",
   name: "Qiqi",
   wiki: "https://genshin-impact.fandom.com/wiki/Qiqi",
-  talentMaterial: Prosperity
+  talentMaterial: Prosperity,
+  commonMaterial: SamachurlScroll
 };
 
 export const Razor: Character = {
   type: "Character",
   name: "Razor",
   wiki: "https://genshin-impact.fandom.com/wiki/Razor",
-  talentMaterial: Resistance
+  talentMaterial: Resistance,
+  commonMaterial: HilichurlMask
 };
 
 export const Sucrose: Character = {
   type: "Character",
   name: "Sucrose",
+
   wiki: "https://genshin-impact.fandom.com/wiki/Sucrose",
-  talentMaterial: Freedom
+  talentMaterial: Freedom,
+  commonMaterial: WhopperflowerNectar
 };
 
 export const Venti: Character = {
   type: "Character",
   name: "Venti",
   wiki: "https://genshin-impact.fandom.com/wiki/Venti",
-  talentMaterial: Ballad
+  talentMaterial: Ballad,
+  commonMaterial: Slime
 };
 
 export const Xiangling: Character = {
   type: "Character",
   name: "Xiangling",
   wiki: "https://genshin-impact.fandom.com/wiki/Xiangling",
-  talentMaterial: Diligence
+  talentMaterial: Diligence,
+  commonMaterial: Slime
 };
 
 export const Xingqiu: Character = {
   type: "Character",
   name: "Xingqiu",
   wiki: "https://genshin-impact.fandom.com/wiki/Xingqiu",
-  talentMaterial: Gold
+  talentMaterial: Gold,
+  commonMaterial: HilichurlMask
 };
 
 export const Klee: Character = {
   type: "Character",
   name: "Klee",
   wiki: "https://genshin-impact.fandom.com/wiki/Klee",
-  talentMaterial: Freedom
+  talentMaterial: Freedom,
+  commonMaterial: SamachurlScroll
 };
 
 export const Characters = [
   Amber,
   Barbara,
   Beidou,
-
   Bennett,
   Chongyun,
   Diluc,
---
+++ src/db/commonMaterials.tsx
@@ -0,0 +1,99 @@
+export type CommonMaterial = {
+  type: "Common Material";
+  name: string;
+  wiki: string;
+};
+
+export const Slime: CommonMaterial = {
+  type: "Common Material",
+  name: "Slime",
+  wiki: "https://genshin-impact.fandom.com/wiki/Slime_Condensate"
+};
+
+export const HilichurlMask: CommonMaterial = {
+  type: "Common Material",
+  name: "Hilichurl Mask",
+  wiki: "https://genshin-impact.fandom.com/wiki/Damaged_Mask"
+};
+
+export const HilichurlArrowhead: CommonMaterial = {
+  type: "Common Material",
+  name: "Hilichurl Arrowhead",
+  wiki: "https://genshin-impact.fandom.com/wiki/Firm_Arrowhead"
+};
+
+export const SamachurlScroll: CommonMaterial = {
+  type: "Common Material",
+  name: "Samachurl Scroll",
+  wiki: "https://genshin-impact.fandom.com/wiki/Divining_Scroll"
+};
+
+export const HilichurlHorn: CommonMaterial = {
+  type: "Common Material",
+  name: "Hilichurl Horn",
+  wiki: "https://genshin-impact.fandom.com/wiki/Heavy_Horn"
+};
+
+export const LeyLine: CommonMaterial = {
+  type: "Common Material",
+  name: "Ley Line",
+  wiki: "https://genshin-impact.fandom.com/wiki/Dead_Ley_Line_Branches"
+};
+
+export const ChaosPart: CommonMaterial = {
+  type: "Common Material",
+  name: "Chaos Part",
+  wiki: "https://genshin-impact.fandom.com/wiki/Chaos_Device"
+};
+
+export const MistGrass: CommonMaterial = {
+  type: "Common Material",
+  name: "Mist Grass",
+  wiki: "https://genshin-impact.fandom.com/wiki/Mist_Flower_Pollen"
+};
+
+export const FatuiKnife: CommonMaterial = {
+  type: "Common Material",
+  name: "Fatui Knife",
+  wiki: "https://genshin-impact.fandom.com/wiki/Hunter%27s_Sacrificial_Knife"
+};
+
+export const FatuiInsignia: CommonMaterial = {
+  type: "Common Material",
+  name: "Fatui Insignia",
+  wiki: "https://genshin-impact.fandom.com/wiki/Recruit%27s_Insignia"
+};
+
+export const TreasureHoarderInsignia: CommonMaterial = {
+  type: "Common Material",
+  name: "Treasure Hoarder Insignia",
+  wiki: "https://genshin-impact.fandom.com/wiki/Treasure_Hoarder_Insignia"
+};
+
+export const BoneShard: CommonMaterial = {
+  type: "Common Material",
+  name: "Bone Shard",
+  wiki: "https://genshin-impact.fandom.com/wiki/Fragile_Bone_Shard"
+};
+
+export const WhopperflowerNectar: CommonMaterial = {
+  type: "Common Material",
+  name: "Whopperflower Nectar",
+  wiki: "https://genshin-impact.fandom.com/wiki/Whopperflower_Nectar"
+};
+
+export const CommonMaterials = [
+  Slime,
+  HilichurlMask,
+  HilichurlArrowhead,
+  SamachurlScroll,
+  HilichurlHorn,
+  LeyLine,
+  ChaosPart,
+  MistGrass,
+  FatuiKnife,
+  FatuiInsignia,
+  TreasureHoarderInsignia,
+  BoneShard,
+  WhopperflowerNectar
+];
