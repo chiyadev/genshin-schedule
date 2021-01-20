1996ea620d1f85036e7a9f99181195fe1ee0b2ed
lauslim12 <nicholasdwiarto@yahoo.com>
2021-01-20 21:37:49 +0700

    Add weekly talent materials in the DB

--- web/db/talentMaterials.ts
+++ web/db/talentMaterials.ts
@@ -49,4 +49,83 @@ export const Gold: TalentMaterial = {
   item: "Teachings of Gold",
 };
 
-export const TalentMaterials = [Freedom, Prosperity, Resistance, Diligence, Ballad, Gold];
+export const DvalinPlume: TalentMaterial = {
+  type: "Talent Level-Up Material",
+  name: "Dvalin's Plume",
+  wiki: "https://genshin-impact.fandom.com/wiki/Dvalin%27s_Plume",
+  item: "Dvalin's Plume",
+};
+
+export const DvalinSigh: TalentMaterial = {
+  type: "Talent Level-Up Material",
+  name: "Dvalin's Sigh",
+  wiki: "https://genshin-impact.fandom.com/wiki/Dvalin%27s_Sigh",
+  item: "Dvalin's Sigh",
+};
+
+export const DvalinClaw: TalentMaterial = {
+  type: "Talent Level-Up Material",
+  name: "Dvalin's Claw",
+  wiki: "https://genshin-impact.fandom.com/wiki/Dvalin%27s_Claw",
+  item: "Dvalin's Claw",
+};
+
+export const TailOfBoreas: TalentMaterial = {
+  type: "Talent Level-Up Material",
+  name: "Tail of Boreas",
+  wiki: "https://genshin-impact.fandom.com/wiki/Tail_of_Boreas",
+  item: "Tail of Boreas",
+};
+
+export const RingOfBoreas: TalentMaterial = {
+  type: "Talent Level-Up Material",
+  name: "Ring of Boreas",
+  wiki: "https://genshin-impact.fandom.com/wiki/Ring_of_Boreas",
+  item: "Ring of Boreas",
+};
+
+export const SpiritLocketOfBoreas: TalentMaterial = {
+  type: "Talent Level-Up Material",
+  name: "Spirit Locket of Boreas",
+  wiki: "https://genshin-impact.fandom.com/wiki/Spirit_Locket_of_Boreas",
+  item: "Spirit Locket of Boreas",
+};
+
+export const TuskOfMonocerosCaeli: TalentMaterial = {
+  type: "Talent Level-Up Material",
+  name: "Tusk of Monoceros Caeli",
+  wiki: "https://genshin-impact.fandom.com/wiki/Tusk_of_Monoceros_Caeli",
+  item: "Tusk of Monoceros Caeli",
+};
+
+export const ShardOfAFoulLegacy: TalentMaterial = {
+  type: "Talent Level-Up Material",
+  name: "Shard of a Foul Legacy",
+  wiki: "https://genshin-impact.fandom.com/wiki/Shard_of_a_Foul_Legacy",
+  item: "Shard of a Foul Legacy",
+};
+
+export const ShadowOfTheWarrior: TalentMaterial = {
+  type: "Talent Level-Up Material",
+  name: "Shadow of the Warrior",
+  wiki: "https://genshin-impact.fandom.com/wiki/Shadow_of_the_Warrior",
+  item: "Shadow of the Warrior",
+};
+
+export const TalentMaterials = [
+  Freedom,
+  Prosperity,
+  Resistance,
+  Diligence,
+  Ballad,
+  Gold,
+  DvalinPlume,
+  DvalinSigh,
+  DvalinClaw,
+  TailOfBoreas,
+  RingOfBoreas,
+  SpiritLocketOfBoreas,
+  TuskOfMonocerosCaeli,
+  ShardOfAFoulLegacy,
+  ShadowOfTheWarrior,
+];
