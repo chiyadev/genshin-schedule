53ef4984361ea0f0736e75152a22bd0e803d3462
lauslim12 <nicholasdwiarto@yahoo.com>
2021-05-10 23:05:05 +0700

    Implement Bahasa Indonesia translation

--- web/langs/index.ts
+++ web/langs/index.ts
@@ -1,18 +1,22 @@
 import EnUS from "./en-US.json";
+import idID from "./id-ID.json";
 // import ZhCN from "./zh-CN.json";
 
-export type Language = "en-US";
+export type Language = "en-US" | "id-ID";
 export const Languages: Language[] = [
   "en-US",
+  "id-ID",
   // "zh-CN",
 ];
 
 export const LanguageNames: Record<Language, string> = {
   "en-US": "English (US)",
+  "id-ID": "Bahasa Indonesia (Indonesia)",
   // "zh-CN": "中文",
 };
 
 export const Localizations: Record<Language, any> = {
   "en-US": EnUS,
+  "id-ID": idID,
   // "zh-CN": { ...EnUS, ...ZhCN },
 };
