cc579fe4e494d9a39bc7c9b14632063ff0373d00
phosphene47 <phosphene47@chiya.dev>
2021-05-15 19:20:54 +1000

    Generate indonesian translation

--- web/langs/index.ts
+++ web/langs/index.ts
@@ -1,17 +1,21 @@
-// import ZhCN from "./zh-CN.json";
+import Id from "./id.json";
+// import ZhHans from "./zh-CN.json";
 
-export type Language = "en-US";
+export type Language = "en-US" | "id";
 export const Languages: Language[] = [
   "en-US",
+  "id",
   // "zh-CN",
 ];
 
 export const LanguageNames: Record<Language, string> = {
   "en-US": "English (US)",
+  id: "Bahasa Indonesia",
   // "zh-CN": "中文",
 };
 
 export const Localizations: Record<Language, any> = {
   "en-US": {},
-  // "zh-CN": { ...EnUS, ...ZhCN },
+  id: Id,
+  // "zh-CN": ZhHans,
 };
