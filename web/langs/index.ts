386ad4b934d1df70f835a7ac3e65d175b6648ab7
phosphene47 <phosphene47@chiya.dev>
2021-07-04 21:10:28 +1000

    Updated Chinese language file from weblate

--- web/langs/index.ts
+++ web/langs/index.ts
@@ -1,21 +1,20 @@
 import Id from "./id.json";
-// import ZhHans from "./zh-CN.json";
+import ZhHans from "./zh_Hans.json";
+import NbNO from "./nb_NO.json";
 
-export type Language = "en-US" | "id";
-export const Languages: Language[] = [
-  "en-US",
-  "id",
-  // "zh-CN",
-];
+export type Language = "en-US" | "id" | "zh-Hans" | "nb-NO";
+export const Languages: Language[] = ["en-US", "id", "zh-Hans", "nb-NO"];
 
 export const LanguageNames: Record<Language, string> = {
   "en-US": "English (US)",
   id: "Bahasa Indonesia",
-  // "zh-CN": "中文",
+  "zh-Hans": "中文",
+  "nb-NO": "[Preview] Bokmål",
 };
 
 export const Localizations: Record<Language, any> = {
   "en-US": {},
   id: Id,
-  // "zh-CN": ZhHans,
+  "zh-Hans": ZhHans,
+  "nb-NO": NbNO,
 };
