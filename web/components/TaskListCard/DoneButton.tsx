eaf34ee7d04260590ec78eb5fc6f4d496e223fe5
BiliGoldenWater <2439577268@qq.com>
2021-08-26 00:37:03 +0800

    merge to one class

--- web/components/TaskListCard/DoneButton.tsx
+++ web/components/TaskListCard/DoneButton.tsx
@@ -1,32 +1,44 @@
-import React, { Dispatch, memo, SetStateAction } from "react";
-import { Task, useConfig } from "../../utils/config";
-import { trackEvent } from "../../utils/umami";
-import { Icon, IconButton, Tooltip } from "@chakra-ui/react";
-import { useTaskDoneSetter, useTaskFocusSetter } from "../../utils/tasks";
-import { FormattedMessage, useIntl } from "react-intl";
-import { Check } from "react-feather";
+import React, {Dispatch, memo, SetStateAction} from "react";
+import {Task, useConfig} from "../../utils/config";
+import {trackEvent} from "../../utils/umami";
+import {Icon, IconButton, Tooltip} from "@chakra-ui/react";
+import {useTaskDoneSetter, useTaskFocusSetter} from "../../utils/tasks";
+import {FormattedMessage, useIntl} from "react-intl";
+import {Check, X} from "react-feather";
+import {useServerTime} from "../../utils/time";
 
-const DoneButton = ({ task, setTask }: { task: Task; setTask: Dispatch<SetStateAction<Task>> }) => {
-  const { formatMessage } = useIntl();
+const DoneButton = ({task, setTask}: { task: Task; setTask: Dispatch<SetStateAction<Task>> }) => {
+  const {formatMessage} = useIntl();
+  const time = useServerTime(60000);
   const [compact] = useConfig("taskListCompact");
 
   const setDone = useTaskDoneSetter(setTask);
   const setFocused = useTaskFocusSetter();
+  const isDue = task.dueTime <= time.valueOf();
 
   return (
-    <Tooltip label={<FormattedMessage defaultMessage="Mark as done" />}>
+    <Tooltip label={isDue ?
+      <FormattedMessage defaultMessage="Mark as done"/> :
+      <FormattedMessage defaultMessage="Mark as to-do"/>
+    }>
       <IconButton
         variant="outline"
-        colorScheme="green"
+        colorScheme={isDue ? "green" : "red"}
         size={compact ? "xs" : "sm"}
         borderRadius="full"
-        icon={<Icon as={Check} fontSize="lg" />}
-        aria-label={formatMessage({ defaultMessage: "Mark as done" })}
+        icon={<Icon as={isDue ? Check : X} fontSize="lg"/>}
+        aria-label={isDue ?
+          formatMessage({defaultMessage: "Mark as done"}) :
+          formatMessage({defaultMessage: "Mark as to-do"})
+        }
         onClick={() => {
-          setDone(true);
+          setDone(isDue);
           setFocused(task);
 
-          trackEvent("taskList", "taskDone");
+          if (isDue)
+            trackEvent("taskList", "taskDone");
+          else
+            trackEvent("taskList", "taskUndone");
         }}
       />
     </Tooltip>
