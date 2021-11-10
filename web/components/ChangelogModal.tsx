433db9d7eaf6af27bc0c0a06c0093661f5f5145e
phosphene47 <phosphene47@chiya.dev>
2021-11-10 19:47:01 +1100

    Update changelog for character image consistency improvements

--- web/components/ChangelogModal.tsx
+++ web/components/ChangelogModal.tsx
@@ -25,6 +25,7 @@ function buildChangelog() {
   return [
     <ChangelogSection key={34} date="2021/11/10">
       <ChangelogItem github={100}>Added 2.1 and 2.2 characters, weapons and artifacts.</ChangelogItem>
+      <ChangelogItem github={101}>Updated character images in customize page to be more consistent.</ChangelogItem>
     </ChangelogSection>,
     <ChangelogSection key={33} date="2021/08/28">
       <ChangelogItem github={96}>Added buttons to show done tasks in task list.</ChangelogItem>
